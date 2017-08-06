eb62e001a5e2e37ec9773a2148450a927cc6d071
Anand Varkey Philips <anandvarkeyphilips@outlook.com>
2017-08-07 00:05:02 +0530

    Initial Setup
    Spring Boot Tutorial  Initial Commit

---
+++ src/main/java/javabrains/springBootStarter/topic/Topic.java
@@ -0,0 +1,35 @@
+package javabrains.springBootStarter.topic;
+
+public class Topic {
+	private int id;
+	private String name;
+	private String desription;
+	
+	
+	public Topic(int id, String name, String desription) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.desription = desription;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesription() {
+		return desription;
+	}
+	public void setDesription(String desription) {
+		this.desription = desription;
+	}
+
+}
